2ea2d7a7a5c38809f4672034e5418dc791968200
Anna Volfman <aniya_07@ukr.net>
2023-11-03 12:35:17 +0200

    add tasks 7, 8 generic

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -10,7 +10,10 @@ export enum UserRole {
 }
 
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
+
+type RoleDescription = Record<UserRole, string>
+
+const RoleDescription: RoleDescription = {
   admin: 'Admin User',
   editor: 'Editor User',
   guest: 'Guest User',
